d9c5a4228f29a9ee07272888d67877fe1c22479c
Nino Beke <nino.beke@vub.be>
2026-05-07 17:45:43 +0200

    Delete maps.py

--- maps.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from level_1 import MAP as L1
-from level_2 import MAP as L2
-from level_3 import MAP as L3
-from level_4 import MAP as L4
-
-ALL_LEVELS = [L1, L2, L3, L4]   
